7e980cb902ba27cfa7e200a74a5cc990acfff798
irlanvl <amandavianiva@gmail.com>
2022-04-21 21:24:22 +0700

    setting up laravel api and first versia

---
+++ app/Http/Controllers/API/ImageController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Image;
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $images = Image::all();
+        return response()->json($images);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'link' => 'required'
+        ]);
+
+        $newImage = new Image([
+            'name' => $request->get('name'),
+            'link' => $request->get('link')
+        ]);
+
+        $newImage->save();
+
+        return response()->json($newImage);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $image = Image::findOrFail($id);
+        return response()->json($image);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $image = Image::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required',
+            'link' => 'required'
+        ]);
+
+        $image->name = $request->get('name');
+        $image->link = $request->get('link');
+
+        $image->save();
+
+        return response()->json($image);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $image = Image::findOrFail($id);
+        $image->delete();
+
+        return response()->json($image::all());
+    }
+}
